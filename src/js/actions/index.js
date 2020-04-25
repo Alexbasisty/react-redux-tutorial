f6824dc71e98df705d916f38d40e5eaaf79296e9
Alex Basystyi <alexbasisty@gmail.com>
2020-04-25 14:56:12 +0200

    getData method with dispatch

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -9,7 +9,7 @@ export const getData = () => {
         return fetch("https://jsonplaceholder.typicode.com/posts")
             .then((response) => response.json())
             .then((json) => {
-                return { type: "DATA_LOADED", payload: json };
+                dispatch({ type: "DATA_LOADED", payload: json });
             });
     };
 };
